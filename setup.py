43d2212edee8ca99afa7645d8566baf5e8403609
Yusaku ONO <yono@eva.ie.u-ryukyu.ac.jp>
2009-11-25 17:24:23 +0900

    setup.py の記述間違いを修正

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ from distutils.core import setup
 
 setup(name='MetaData',
       version='0.1',
-      py_modules=['metadata','co_word','htmlutil','htmltutil_ext',
+      py_modules=['metadata','co_word','htmlutil','htmlutil_ext',
                   'extractword','extractword_ext']
       )
